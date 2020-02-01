418ee2634c8aa7075e6eb339f2be73ae0ffa8b7c
Eric Laursen <ericjlaursen@gmail.com>
2020-02-01 17:01:46 -0500

    work

--- server.js
+++ server.js
@@ -5,17 +5,40 @@ const jwt = require('jsonwebtoken')
 //would usually come from somewhere else
 let dummyKey = 'secretKeyStringHere!'
 let expTime = Math.floor(Date.now() / 1000) + (60 * 60)
-console.log('expTime')
-console.log(expTime)
 
 
+const refreshToken = () => {
+
+}
+//an un-protected route
 app.get('/open', (req,res) => {
 	return res.json({'open': 'endpoint'})
 })
 
+/*
+	gets a token
+	REQUIRES username && password
+*/
+
+
+const checkForValidToken = (req,res) => {
+	const { token } = req.headers
+	if(!token){
+		return res.json({'Error': 'missing token'})
+	}
+
+}
 app.get('/token', (req,res) => {
+
+	//parse username && password from request url params
 	let {un, pw} = req.query
 
+	//error handling
+	if(!un || !pw){
+		return res.json({"Missing": "required params"})
+	}
+
+	//build the jwt
 	let token = jwt.sign({
 	  exp: expTime,
 	  data: {
@@ -26,13 +49,18 @@ app.get('/token', (req,res) => {
 	  }
 	}, dummyKey);
 
-	if(!un || !pw){
-		return res.json({"Missing": "required params"})
-	}
-
-
+	//return the jwt
+	//... can be appended to res header ....
 	return res.json({'token': token})
 })
+
+app.get('/protected', checkForValidToken, (req,res) => {
+	res.json({'passed': 'Checked for token && passed!'})
+})
+
+// app.get('/protectedWithRefresh', refreshToken(checkForValidToken), (req,res) => {
+// 	res.json({'passed': 'Checked for token && passed!'})
+// })
 
 let listenPort = process.env.PORT || 1234
 app.listen(listenPort, () => {
